ed7686ca6ba25dcffe2b4c96d8859d345a5e68cd
Egor <bifrost.step@gmail.com>
2021-06-05 22:57:29 +0300

    Добавил тесты, обновил зависимости

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -2,36 +2,113 @@
  * @fileoverview Rule to swap lodash map method on Array with native Array#map
  * @author Super-Cereal
  */
-"use strict";
+'use strict';
 
-//------------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/map"),
+const rule = require('../../../lib/rules/map');
+const RuleTester = require('eslint').RuleTester;
 
-    RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("map", rule, {
+const ruleTester = new RuleTester({parserOptions: {ecmaVersion: 2018}});
+/* eslint-disable max-len */
+ruleTester.run('map', rule, {
+  valid: [
+    // При переопределении "_" правило не должно срабатывать после переопределения(1)
+    `let _ = require('lodash');
+    _ = {map: (n) => n * n};
+    _.map([1, 2, 3], (n) => n * n);
+    `,
 
-    valid: [
+    // Если первый аргумент - переменная с обьектом, то правило не должно срабатывать
+    `let _ = require('lodash');
+    let x = {1: 1, 2: 2, 3: 3};
+    _.map(x, (n) => n * n);
+    `,
 
-        // give me some code that won't trigger a warning
-    ],
+    // Если первый аргумент - обьект, то правило не должно срабатывать
+    `let _ = require('lodash');
+    _.map({1: 1, 2: 2, 3: 3}, (n) => n * n);
+    `,
+  ],
 
-    invalid: [
-        {
-            code: "_.map(Array, func())",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+  invalid: [
+    // Если первый аргумент - массив, правило должно сработать
+    {
+      code: `
+        let _ = require('lodash');
+        _.map([1, 2, 3], (n) => n * n);
+      `,
+      errors: [{message: 'Better to use native Array#map'}],
+      output: `
+        let _ = require('lodash');
+        [1, 2, 3].map((n) => n * n);
+      `,
+    },
+
+    // Если _.map(Array, fn()) растягивается на несколько строк,
+    // а правило фиксит ошибку, то не возникает лишних ошибок
+    {
+      code: `
+        let _ = require('lodash');
+        _.map([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 10, 9, 1, 12, 13], 
+          (n) => n * n);
+      `,
+      errors: [{message: 'Better to use native Array#map'}],
+      output: `
+        let _ = require('lodash');
+        [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 10, 9, 1, 12, 13].map((n) => n * n);
+      `,
+    },
+
+    // Если первый аргумент - перменная с массивом, правило должно сработать
+    {
+      code: `
+        let _ = require('lodash');
+        let x = [1, 2, 3];
+        _.map(x, (n) => n * n);
+      `,
+      errors: [{message: 'Better to use native Array#map'}],
+      output: `
+        let _ = require('lodash');
+        let x = [1, 2, 3];
+        x.map((n) => n * n);
+      `,
+    },
+
+    // Если первый аргумент - пустой массив, правило должно работать
+    {
+      code: `
+        let _ = require('lodash');
+        _.map([], (n) => n * n);
+      `,
+      errors: [{message: 'Better to use native Array#map'}],
+      output: `
+        let _ = require('lodash');
+        [].map((n) => n * n);
+      `,
+    },
+
+    // При переопределении "_" правило не должно срабатывать после переопределения(2)
+    {
+      code: `
+        let _ = require('lodash');
+        _.map([1, 2, 3], (n) => n * n);
+        _ = {map: (n) => n * n};
+        _.map([1, 2, 3], (n) => n * n);
+      `,
+      errors: [{message: 'Better to use native Array#map'}],
+      output: `
+        let _ = require('lodash');
+        [1, 2, 3].map((n) => n * n);
+        _ = {map: (n) => n * n};
+        _.map([1, 2, 3], (n) => n * n);
+      `,
+    },
+  ],
 });
